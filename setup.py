ba917d5e758d9892bae573f8456b0076d4aa4d10
Alexander Heavner <alex@zope.com>
2007-02-07 22:21:31 +0000

    The __init__.py file was added to make 'zope' a namespace package The setup.py file was added so that the package can be 'eggified'

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zope.locking",
+    version="0.1dev",
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['zope'],
+    include_package_data=True,
+    install_requirements = ['setuptools'],
+    zip_safe = False
+    )

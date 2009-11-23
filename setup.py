5dcabf6acbb6a67800872f0c5401c3ee21e26754
Patrick Strawderman <patrick@kilink.net>
2009-11-23 21:07:25 +0000

    Prepare for release: 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zope.locking",
-    version="1.1b",
+    version="1.2",
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['zope'],

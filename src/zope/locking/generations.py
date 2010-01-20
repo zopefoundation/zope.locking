c3797463abf9e1b008636e083b74bfe608950cd4
Patrick Strawderman <patrick@kilink.net>
2010-01-20 15:29:21 +0000

    The generation added in 1.2 didn't go far enough in cleaning up token utilities

--- src/zope/locking/generations.py
+++ src/zope/locking/generations.py
@@ -3,14 +3,15 @@ import zope.interface
 import zope.app.generations.interfaces
 
 import zope.locking.interfaces
+import zope.locking.utils
 
 class SchemaManager(object):
 
     zope.interface.implements(
         zope.app.generations.interfaces.IInstallableSchemaManager)
 
-    minimum_generation = 1
-    generation = 1
+    minimum_generation = 2
+    generation = 2
 
     def install(self, context):
         # Clean up cruft in any existing token utilities.
@@ -22,7 +23,13 @@ class SchemaManager(object):
                 fix_token_utility(util)
 
     def evolve(self, context, generation):
-        pass
+        if generation == 2:
+            # Going from generation 1 -> 2, we need to run the token
+            # utility fixer again because of a deficiency it had in 1.2.
+            app = context.connection.root().get('Application')
+            if app is not None:
+                for util in find_token_utilities(app):
+                    fix_token_utility(util)
 
 
 schemaManager = SchemaManager()
@@ -58,5 +65,18 @@ def fix_token_utility(util):
             util._principal_ids[pid] = new_tree
         else:
             del util._principal_ids[pid]
+    now = zope.locking.utils.now()
     for dt, tree in list(util._expirations.items()):
-        util._expirations[dt] = BTrees.OOBTree.OOTreeSet(tree)
+        if dt > now:
+            util._expirations[dt] = BTrees.OOBTree.OOTreeSet(tree)
+        else:
+            del util._expirations[dt]
+            for token in tree:
+                # Okay, we could just adapt token.context to IKeyReference
+                # here...but we don't want to touch token.context,
+                # because some wonky objects need a site set before
+                # they can be unpickled.
+                for key_ref, (_token, _, _) in list(util._locks.items()):
+                    if token is _token:
+                        del util._locks[key_ref]
+                        break

8a1df57de4ba6376a9f53c39182a89828b1f8830
Alex Smith <alex@zope.com>
2011-01-31 16:22:11 +0000

    version info

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zope.locking",
-    version="1.3dev",
+    version="1.2.2",
     license='ZPL 2.1',
     packages=find_packages('src'),
     package_dir={'':'src'},

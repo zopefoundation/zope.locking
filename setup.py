2152cff3ca991f3540bb6704aeaf5f1295470fdc
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-19 13:12:18 +0100

    start fixing up this package so we can move it back under the zopefoundation umbrella

--- setup.py
+++ setup.py
@@ -1,21 +1,44 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.locking package
+"""
 from setuptools import setup, find_packages
+
 
 version = '1.3+md2.dev0'
 
-tests_require=[
-    'zope.app.appsetup',
+
+tests_require = [
     'transaction',
+    'zope.app.appsetup',
     ]
+
 
 setup(
     name="zope.locking",
     version=version,
     license='ZPL 2.1',
     packages=find_packages('src'),
-    package_dir={'':'src'},
+    package_dir={'': 'src'},
     namespace_packages=['zope'],
     include_package_data=True,
-    install_requires = [
+    install_requires=[
         'BTrees',
         'persistent',
         'pytz',
@@ -29,7 +52,7 @@ setup(
         'zope.schema',
         'zope.security',
         ],
-    zip_safe = False,
+    zip_safe=False,
     tests_require=tests_require,
     extras_require={'test': tests_require},
     description=open("README.txt").read(),
